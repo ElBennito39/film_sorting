003f3f0c00c1c4dd801e17b387c94b6508e50479
Sidekick <brienbennett@hotmail.com>
2022-06-30 13:33:22 -0400

    added symlink creation

--- Game Footage Organizer.py
+++ Game Footage Organizer.py
@@ -15,39 +15,47 @@
 # finding files with one play attribute tag
 from ctypes.wintypes import tagMSG
 import os
-from mac_alias import Alias
 
-# build dictionary of play types and alias destination folders
+#path placeholder for video link destinations
+
+DEST_PATH = "/Users/Sidekick/Desktop/PHC 15U/Post Scout"
+VID_PATH= "/Users/Sidekick/Desktop/PHT 15U/Video Library"
+
+# build dictionary of play types and symlink destination folders
 play_definition = {
-    "DZC":"/Users/Sidekick/Desktop/PHC 15U/Post Scout/DZCoverage",
-    "BO":"/Users/Sidekick/Desktop/PHC 15U/Post Scout/Breakout (BO)",
-    "NZF":"/Users/Sidekick/Desktop/PHC 15U/Post Scout/NZForecheck",
+    "DZC":"{}/DZCoverage".format(DEST_PATH),
+    "BO":"{}/Breakout (BO)".format(DEST_PATH),
+    "NZF":"{}/NZForecheck".format(DEST_PATH),
 }
     
 
-# return a list of full file paths with given play_tag
-def find_tag(play_tag):
+# return a list of full file names with given play_tag
+def find_tags(play_tag):
     tag_results = []
-    for path, currentDirectory, files in os.walk("/Users/Sidekick/Desktop/PHC 15U"):
+    for path, currentDirectory, files in os.walk(VID_PATH):
        for file in files:
         if play_tag in file:
-            # tag_results.append(path+'/'+file)
-            tag_results.append(file)
+             tag_results.append(file)
     return tag_results
     print (tag_results)
+# list of all found tags as defined by tag_results
 
-list = find_tag ("DZC")
+list = find_tags ("DZC")
 
 # take in tag results, create alias to each path in that tags directory
 
-def create_aliases(file_name):
-    # file_path = '02_13_BO_DZC_NZF.mp4'
+def create_symlink(file_name):
+
     file_name = file_name.replace('.mp4','')
     
     tag_list = file_name.split('_')
         
     for tag in tag_list:
-        Alias.for_file(play_definition[tag])
+        if tag in play_definition:
+            print (tag)
+            os.symlink("{}/Video Library/06-24-2022_Game#2_TidalWave".format(VID_PATH)+"/"+file_name+".mp4","/Users/Sidekick/Desktop/{}".format(file_name)+".mp4")
+
+            
 
 #
 # create aliases from list of files
@@ -58,4 +66,3 @@ def bulk_create_aliases(list):
 
 bulk_create_aliases(list)
 
-# get python manager for mac_alias to work
