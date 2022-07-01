6d6f006fa4aff5eee204e6d2f28f92a394541215
nichol88 <nichol88@gmail.com>
2022-07-01 00:57:11 -0400

    add todo list and extract constants

---
+++ constants.py
@@ -0,0 +1,10 @@
+# path placeholders for video link destinations
+VID_PATH  = "/Users/Sidekick/Desktop/PHT 15U/Video Library"
+DEST_PATH = "/Users/Sidekick/Desktop/PHC 15U/Post Scout"
+
+# build dictionary of play types and symlink destination folders
+play_definition = {
+    "DZC":"{}/DZCoverage".format(DEST_PATH),
+    "BO":"{}/Breakout (BO)".format(DEST_PATH),
+    "NZF":"{}/NZForecheck".format(DEST_PATH),
+}
