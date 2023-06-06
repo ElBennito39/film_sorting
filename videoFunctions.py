f8e6c41f29e4ec20ba92e406322fc00c40925819
Brien Bennett <brienbennett@hotmail.com>
2023-06-06 18:48:17 -0400

    update open directory dialog box. add undo playlist action functionality. cleanup

--- videoFunctions.py
+++ videoFunctions.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QFileDialog, QButtonGroup
+from PySide2.QtWidgets import QFileDialog, QDesktopWidget, QDialog
 from PySide2.QtMultimedia import QMediaContent, QMediaPlayer
 from PySide2.QtCore import QUrl, Qt
 from datetime import timedelta
@@ -6,37 +6,39 @@ import os
 
 ##FUNCTIONS BELOW
 
-#function to open video files in directories and their subdirectories and put them in a playlist
+#Function to open video files in directories and their subdirectories and put them in a playlist
 def open_directory(self):
-    dir_name = QFileDialog.getExistingDirectory(self, "Open Directory")
-    if dir_name:
-        # Find all video files in the directory and its subdirectories
-        for root, dirs, files in os.walk(dir_name):
-            for file_name in files:
-                if file_name.endswith((".mp4", ".mov", ".mkv", ".avi", ".ts")):  # Add other video formats as needed
-                    absolute_path = os.path.join(root, file_name)
+    file_dialog = QFileDialog(self, "Open Directory")
+    file_dialog.setFileMode(QFileDialog.DirectoryOnly)
+    file_dialog.setOption(QFileDialog.DontUseNativeDialog)
 
-                    # Create a playlist item with up to two directories and the file name
-                    playlist_item = "/".join(absolute_path.split("/")[-3:])
-                    
-                    # Only add new item if it's not already in the playlist
-                    if playlist_item not in self.file_paths:
-                        self.file_paths[playlist_item] = absolute_path  # Add the absolute path to the dictionary
-                        self.playlist.addItem(playlist_item)
-        # # After opening the directory and populating the playlist
-        # self.playlist.setFocus()  # Set focus to the playlist widget
-        # self.playlist.setCurrentRow(0)  # Optionally, select the first item in the playlist
+    # Define your desired size here
+    file_dialog.resize(1200, 800)
 
-                        # # Read the metadata of the file and load the tags
-                        # media_content = QMediaContent(QUrl.fromLocalFile(absolute_path))
-                        # tag_string = media_content.metaData("comments")
+    # Centering file dialog
+    qr = file_dialog.frameGeometry()
+    cp = QDesktopWidget().availableGeometry().center()
+    qr.moveCenter(cp)
+    file_dialog.move(qr.topLeft())
 
-                        # if tag_string:
-                        #     try:
-                        #         tag_dict = json.loads(tag_string)
-                        #         # TODO: Update the UI to display the loaded tags
-                        #     except json.JSONDecodeError:
-                        #         print(f"Failed to parse tags for file: {absolute_path}")
+
+    if file_dialog.exec_() == QFileDialog.Accepted:
+        dir_name = file_dialog.selectedFiles()[0]
+
+        if dir_name:
+            # Find all video files in the directory and its subdirectories
+            for root, dirs, files in os.walk(dir_name):
+                for file_name in files:
+                    if file_name.endswith((".mp4", ".mov", ".mkv", ".avi", ".ts")):  # Add other video formats as needed
+                        absolute_path = os.path.join(root, file_name)
+
+                        # Create a playlist item with up to two directories and the file name
+                        playlist_item = "/".join(absolute_path.split("/")[-3:])
+
+                        # Only add new item if it's not already in the playlist
+                        if playlist_item not in self.file_paths:
+                            self.file_paths[playlist_item] = absolute_path  # Add the absolute path to the dictionary
+                            self.playlist.addItem(playlist_item)
 
 
 # Function to change the video file to play
@@ -72,13 +74,74 @@ def change_video(self, item):
             # Set the label's text to the absolute path
             self.file_path_label.setText(absolute_path)
 
+# Function to remove the currently selected items from the playlist
+def remove_current_item(list_widget, file_paths):
+    selected_items = list_widget.selectedItems()
+
+    # Create a list of rows from the selected items
+    rows = [list_widget.row(item) for item in selected_items]
+
+    # Sort rows in reverse order
+    rows.sort(reverse=True)
+
+    removed_items = []
+    # Remove items and corresponding file paths
+    for row in rows:
+        item_text = list_widget.takeItem(row).text()
+        file_path = file_paths.pop(item_text, None)
+        removed_items.append((row, item_text, file_path))
+
+    return ('remove', removed_items) if removed_items else None
+
+# Function to add items to the undo stack of actions
+def add_item(list_widget, file_paths, item_text, file_path):
+    # Add the item to the list widget and the file paths dictionary
+    list_widget.addItem(item_text)
+    file_paths[item_text] = file_path
+
+    return ('add', [(list_widget.count()-1, item_text, file_path)])
+
+# # Function to trigger the undo action from the action stack
+# def undo(list_widget, file_paths, undo_stack):
+#     if not undo_stack:
+#         return
+
+#     action, items = undo_stack.pop()
+#     if action == 'remove':
+#         for row, item_text, file_path in reversed(items):
+#             list_widget.insertItem(row, item_text)
+#             file_paths[item_text] = file_path
+#     elif action == 'add':
+#         for row, item_text, file_path in reversed(items):
+#             list_widget.takeItem(row)
+#             file_paths.pop(item_text, None)
+
+# Function to trigger the undo action from the action stack
+def undo(list_widget, file_paths, undo_stack):
+    if not undo_stack:
+        return
+
+    action, items = undo_stack.pop()
+    if action == 'remove':
+        for row, item_text, file_path in reversed(items):
+            # Only add item back if it doesn't already exist in the list
+            if item_text not in file_paths:
+                list_widget.insertItem(row, item_text)
+                file_paths[item_text] = file_path
+    elif action == 'add':
+        for row, item_text, file_path in reversed(items):
+            # Only remove item if it exists in the list
+            if item_text in file_paths:
+                list_widget.takeItem(row)
+                file_paths.pop(item_text, None)
+
+
 # Function to toggle full screen
 def toggle_window_size(self):
     if self.isMaximized():
         self.showNormal()  # Restore window size if it's maximized
     else:
         self.showMaximized()  # Maximize window if it's not already maximized
-
 
 # Function to set the auto-play next setting
 def set_auto_play_next(self, state):
@@ -87,13 +150,6 @@ def set_auto_play_next(self, state):
 def toggle_auto_play_next(self):
     self.auto_play_next_checkbox.setChecked(not self.auto_play_next_checkbox.isChecked())
 
-# # Function to reset the video playback position to '0' once the media finishes playing.
-# def check_media_status(self, status):
-#     if status == QMediaPlayer.EndOfMedia:
-#         self.media_player.setPosition(0)
-#         if self.auto_play_next_checkbox.isChecked():
-#             next_video(self)
-#             self.media_player.play()
 # Function to reset the video playback position to '0' once the media finishes playing.
 def check_media_status(self, status):
     if status == QMediaPlayer.EndOfMedia:
@@ -104,8 +160,6 @@ def check_media_status(self, status):
                 # If not at the last item, go to the next video
                 next_video(self)
                 self.media_player.play()
-
-
 
 # sets the speed of playback
 def change_speed(self, value):
@@ -144,15 +198,6 @@ def rewind(self):
     position = self.media_player.position() - 3000  # Get the current position and subtract 3000 milliseconds from it
     self.media_player.setPosition(max(position, 0))  # Set the new position, don't go below 0
 
-# # Change to next item in the playlist - call change_video to change the video.
-# def next_video(video_window):
-#     current_row = video_window.playlist.currentRow()
-#     if current_row < video_window.playlist.count() - 1:
-#         video_window.playlist.setCurrentRow(current_row + 1)
-#         change_video(video_window, video_window.playlist.currentItem())
-#     else:
-#         video_window.media_player.stop()
-# Change to next item in the playlist - call change_video to change the video.
 def next_video(video_window):
     current_row = video_window.playlist.currentRow()
     total_items = video_window.playlist.count()
@@ -209,23 +254,3 @@ def toggle_widget_visibility(widget):
 def toggle_checkbox(checkbox):
     current_state = checkbox.isChecked()
     checkbox.setChecked(not current_state)
-
-
-
-# def save_tags(self):
-#     # Extract the tag values from your input widgets
-#     # # For example, if you had a QLineEdit for the user to input a title:
-#     # title = self.title_input.text()
-
-#     # Same for other types of tags...
-#     # Create a dictionary with your tag values
-#     tag_dict = {"title": title, {}}
-
-#     # Convert the dictionary to a JSON string
-#     json_string = json.dumps(tag_dict)
-
-#     # Write the JSON string to the file's comments metadata
-#     self.current_video.metadata["comments"] = json_string
-
-#     # Reset the unsaved changes flag
-#     self.unsaved_changes = False
